9511f2ed792111c1a28d6e0e3e2cb855c6754780
gajjard <55995889+gajjard@users.noreply.github.com>
2021-03-27 22:50:17 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,11 @@
+# import pywhatkit module.
+import pywhatkit
+
+# input a image.
+input = "wave.png"
+
+# output a image.
+output = "wave.text"
+
+# This will convert any given image to ASCII art.
+pywhatkit.image_to_ascii_art(input, output)
